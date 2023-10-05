bdf20d2050e78b1afeb2d32b2a7e3969d5cabc68
Oliver Berg <oliver@berg.moe>
2023-10-05 14:54:19 +0200

    Remove old code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,18 @@
 plugins {
-    kotlin("jvm") version "1.9.0"
+    kotlin("jvm") version "1.9.10"
     id("me.him188.maven-central-publish") version "1.0.0-dev-3"
 }
 
 group = "net.ormr.asmkt"
 description = "Kotlin DSL wrapper for ASM"
-version = "0.0.13"
+version = "0.1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    api(group = "org.ow2.asm", name = "asm-commons", version = "9.3")
-    implementation(group = "moe.kanon.krautils", name = "krautils-core", version = "0.0.1")
+    api("org.ow2.asm:asm-commons:9.6")
 }
 
 kotlin {
@@ -31,7 +30,6 @@ tasks {
         kotlinOptions {
             jvmTarget = "17"
             freeCompilerArgs = freeCompilerArgs + listOf(
-                "-opt-in=kotlin.RequiresOptIn",
                 "-opt-in=kotlin.contracts.ExperimentalContracts",
             )
         }
