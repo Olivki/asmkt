9d3b97549a50c50002eeef01b02c8375e63c0903
Oliver Berg <oliver@berg.moe>
2025-07-24 14:48:30 +0200

    Use `MavenDeployer` plugin for publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.mavenDeployer)
 }
 
 group = "net.ormr.asmkt"
@@ -17,7 +18,7 @@ dependencies {
 
 kotlin {
     explicitApi()
-    jvmToolchain(21)
+    jvmToolchain(8)
 }
 
 tasks {
@@ -27,6 +28,41 @@ tasks {
             freeCompilerArgs += "-opt-in=kotlin.contracts.ExperimentalContracts"
             freeCompilerArgs += "-Xcontext-receivers"
             freeCompilerArgs += "-Xjvm-default=all"
+        }
+    }
+}
+
+deployer {
+    content {
+        kotlinComponents {
+            emptyDocs()
+            kotlinSources()
+        }
+    }
+
+    projectInfo {
+        url.set("https://github.com/Olivki/asmkt")
+        license(apache2)
+        developer("Olivki", "oliver@berg.moe")
+
+        scm {
+            fromGithub("Olivki", "asmkt")
+        }
+    }
+
+    localSpec()
+
+    centralPortalSpec {
+        allowMavenCentralSync = false
+
+        auth {
+            user.set(secret("CENTRAL_PORTAL_USER"))
+            password.set(secret("CENTRAL_PORTAL_PASSWORD"))
+        }
+
+        signing {
+            key.set(secret("OSSRH_GPG_SECRET_KEY"))
+            password.set(secret("OSSRH_GPG_SECRET_KEY_PASSWORD"))
         }
     }
 }
